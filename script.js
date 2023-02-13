7a56fa971823f5fd235bf28f9f306148dfe6d119
HassanKun <ev292il@gmail.com>
2023-02-13 20:19:10 +0300

    Complete implmenting playRound fuction

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 
 // make the computer choose a random play
-function getComputerChoice {
+function getComputerChoice() {
     // use random function to genrate one of the three choices
     // temporary make the function return rock always to test out the program
     return "rock"
@@ -10,24 +10,35 @@ function getComputerChoice {
 
 // given two chooses, return True if player wins the game
 function playRound(playerSelection, computerSelection){
-    playerSelection = playerSelection.tolowercase();
+    playerSelection = playerSelection.toLowerCase();
     
     // 2. compare the selection based on the game rules
     // if player rock:
         // computer rock --> Draw!
         // computer paper --> You Lose! Paper beats Rock
         // computer sissors --> You Win! Rock beats Sissors
+    if ( playerSelection === "rock" && computerSelection === "rock" ) { return "Draw!";}
+    if ( playerSelection === "rock" && computerSelection === "paper" ) { return "You Lose! Paper beats Rock";}
+    if ( playerSelection === "rock" && computerSelection === "sissors" ) { return "You Win! Rock beats Sissors";}
     // if player Paper :
         // computer rock --> You Win! Paper beats Rock
         // computer paper --> Draw!
         // computer sissors --> You Lose! Sissors beats Paper
+    if ( playerSelection === "paper" && computerSelection === "rock" ) { return "You Win! Paper beats Rock";}
+    if ( playerSelection === "paper" && computerSelection === "paper" ) { return "Draw!";}
+    if ( playerSelection === "paper" && computerSelection === "sissors" ) { return "You Lose! Sissors beats Paper";}
+
     // if player Sissors:
         // computer rock --> You Lose! Rock beats Sissors
         // computer paper --> You Win! Sissors beats Paper
         // computer sissors --> Draw!
+    if ( playerSelection === "sissors" && computerSelection === "rock" ) { return "You Lose! Rock beats Sissors";}
+    if ( playerSelection === "sissors" && computerSelection === "paper" ) { return "You Win! Sissors beats Paper";}
+    if ( playerSelection === "sissors" && computerSelection === "paper" ) { return "Draw!";}
 
+}
 
-const playerSelection = "rock";
+const playerSelection = "Sissors";
 const computerSelection = getComputerChoice();
 
 console.log(playRound(playerSelection,computerSelection));
