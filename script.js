ccecb380c6b144b1e93597cf55949966698af767
HassanKun <ev292il@gmail.com>
2023-02-13 20:43:15 +0300

    Add new helper function sayTheWinner and change return values of playRound

--- script.js
+++ script.js
@@ -21,28 +21,37 @@ function playRound(playerSelection, computerSelection){
         // computer rock --> Draw!
         // computer paper --> You Lose! Paper beats Rock
         // computer sissors --> You Win! Rock beats Sissors
-    if ( playerSelection === "rock" && computerSelection === "rock" ) { return "Draw!";}
-    if ( playerSelection === "rock" && computerSelection === "paper" ) { return "You Lose! Paper beats Rock";}
-    if ( playerSelection === "rock" && computerSelection === "sissors" ) { return "You Win! Rock beats Sissors";}
+    if ( playerSelection === "rock" && computerSelection === "rock" ) { return 0;}
+    if ( playerSelection === "rock" && computerSelection === "paper" ) { return -1;}
+    if ( playerSelection === "rock" && computerSelection === "sissors" ) { return 1;}
     // if player Paper :
         // computer rock --> You Win! Paper beats Rock
         // computer paper --> Draw!
         // computer sissors --> You Lose! Sissors beats Paper
-    if ( playerSelection === "paper" && computerSelection === "rock" ) { return "You Win! Paper beats Rock";}
-    if ( playerSelection === "paper" && computerSelection === "paper" ) { return "Draw!";}
-    if ( playerSelection === "paper" && computerSelection === "sissors" ) { return "You Lose! Sissors beats Paper";}
+    if ( playerSelection === "paper" && computerSelection === "rock" ) { return 1;}
+    if ( playerSelection === "paper" && computerSelection === "paper" ) { return 0;}
+    if ( playerSelection === "paper" && computerSelection === "sissors" ) { return -1;}
 
     // if player Sissors:
         // computer rock --> You Lose! Rock beats Sissors
         // computer paper --> You Win! Sissors beats Paper
         // computer sissors --> Draw!
-    if ( playerSelection === "sissors" && computerSelection === "rock" ) { return "You Lose! Rock beats Sissors";}
-    if ( playerSelection === "sissors" && computerSelection === "paper" ) { return "You Win! Sissors beats Paper";}
-    if ( playerSelection === "sissors" && computerSelection === "sissors" ) { return "Draw!";}
+    if ( playerSelection === "sissors" && computerSelection === "rock" ) { return -1;}
+    if ( playerSelection === "sissors" && computerSelection === "paper" ) { return 1;}
+    if ( playerSelection === "sissors" && computerSelection === "sissors" ) { return 0;}
 
 }
+
+function sayTheWinner(playerSelection,computerSelection){
+    if (playRound(playerSelection,computerSelection) === 1) { return `You Win, ${playerSelection} beats ${computerSelection}`;}
+    if (playRound(playerSelection,computerSelection) === 0) { return "Draw!";}
+    if (playRound(playerSelection,computerSelection) === -1) { return `You Lose, ${computerSelection} beats ${playerSelection}`;}
+
+}
+
+
 
 const playerSelection = "Sissors";
 const computerSelection = getComputerChoice();
 
-console.log(playRound(playerSelection,computerSelection));
+console.log(sayTheWinner(playerSelection,computerSelection));
