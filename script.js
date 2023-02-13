0857aa47ae29516e8a970565cae7803811c9a3e2
HassanKun <ev292il@gmail.com>
2023-02-13 19:49:35 +0300

    put the skeleton of the soultion and comment it

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+
+// make the computer choose a random play
+function getComputerChoice{
+    // use random function to genrate one of the three choices
+}
+
+// given two chooses, return True if player wins the game
+function playRound(playerSelection, computerSelection){
+    // 1. lower case playerSelection
+    // 2. compare the selection based on the game rules
+}
+
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection,computerSelection));
