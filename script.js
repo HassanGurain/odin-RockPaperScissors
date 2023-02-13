327453b8bf49c3803762100590e43f46e17f507e
HassanKun <ev292il@gmail.com>
2023-02-13 20:04:50 +0300

    put out the solution to playRound function and hardcode getComputerChoice to return rock to test out the program

--- script.js
+++ script.js
@@ -2,15 +2,29 @@
 
 
 // make the computer choose a random play
-function getComputerChoice{
+function getComputerChoice {
     // use random function to genrate one of the three choices
+    // temporary make the function return rock always to test out the program
+    return "rock"
 }
 
 // given two chooses, return True if player wins the game
 function playRound(playerSelection, computerSelection){
-    // 1. lower case playerSelection
+    playerSelection = playerSelection.tolowercase();
+    
     // 2. compare the selection based on the game rules
-}
+    // if player rock:
+        // computer rock --> Draw!
+        // computer paper --> You Lose! Paper beats Rock
+        // computer sissors --> You Win! Rock beats Sissors
+    // if player Paper :
+        // computer rock --> You Win! Paper beats Rock
+        // computer paper --> Draw!
+        // computer sissors --> You Lose! Sissors beats Paper
+    // if player Sissors:
+        // computer rock --> You Lose! Rock beats Sissors
+        // computer paper --> You Win! Sissors beats Paper
+        // computer sissors --> Draw!
 
 
 const playerSelection = "rock";
